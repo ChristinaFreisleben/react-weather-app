8c2441da9e24513eef17271f08ddd058edf62659
Christina <98745726+ChristinaFreisleben@users.noreply.github.com>
2022-07-24 21:51:59 +0200

    Updated temperature

--- src/Forecast.js
+++ src/Forecast.js
@@ -7,23 +7,23 @@ export default function Forecast() {
       <div className="row d-flex justify-content-center">
         <div className="col-lg-2 col-md-6">
           <h5>Monday</h5>
-          <p>31°C | 88°F</p>
+          <p>31°C | 18°C</p>
         </div>
         <div className="col-lg-2 col-md-6">
           <h5>Tuesday</h5>
-          <p>31°C | 88°F</p>
+          <p>31°C | 18°C</p>
         </div>
         <div className="col-lg-2 col-md-6">
           <h5>Wednesday</h5>
-          <p>31°C | 88°F</p>
+          <p>31°C | 18°C</p>
         </div>
         <div className="col-lg-2 col-md-6">
           <h5>Thursday</h5>
-          <p>31°C | 88°F</p>
+          <p>31°C | 18°C</p>
         </div>
         <div className="col-lg-2 col-md-6">
           <h5>Friday</h5>
-          <p>31°C | 88°F</p>
+          <p>31°C | 18°C</p>
         </div>
       </div>
     </div>

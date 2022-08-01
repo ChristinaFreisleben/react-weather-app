d7821ede687b6296aa3933e00dd2102967cbc22a
Christina <98745726+ChristinaFreisleben@users.noreply.github.com>
2022-08-01 20:14:09 +0200

    Fixed forecast grid

--- src/Forecast.js
+++ src/Forecast.js
@@ -2,53 +2,56 @@ import React from "react";
 import "./Forecast.css";
 
 export default function Forecast() {
+  let icon = "https://www.freeiconspng.com/uploads/cloud-icon-8.png";
   return (
     <div className="forecast">
-      <div className="row d-flex justify-content-center">
-        <div className="col-lg-2 col-md-6">
-          <h5>Monday</h5>
-          <img
-            source="https://cdn.dribbble.com/users/98494/screenshots/2714201/sun-icon.jpg"
-            alt="Icon showing a sun and a cloud"
-            className="forecastEmoji"
-          />
-          <p>31°C | 18°C</p>
-        </div>
-        <div className="col-lg-2 col-md-6">
-          <h5>Tuesday</h5>
-          <img
-            source="https://static.thenounproject.com/png/1197734-200.png"
-            alt="Icon showing a sun and a cloud"
-            className="forecastEmoji"
-          />
-          <p>31°C | 18°C</p>
-        </div>
-        <div className="col-lg-2 col-md-6">
-          <h5>Wednesday</h5>
-          <img
-            source="https://static.thenounproject.com/png/1197734-200.png"
-            alt="Icon showing a sun and a cloud"
-            className="forecastEmoji"
-          />
-          <p>31°C | 18°C</p>
-        </div>
-        <div className="col-lg-2 col-md-6">
-          <h5>Thursday</h5>
-          <img
-            source="https://static.thenounproject.com/png/1197734-200.png"
-            alt="Icon showing a sun and a cloud"
-            className="forecastEmoji"
-          />
-          <p>31°C | 18°C</p>
-        </div>
-        <div className="col-lg-2 col-md-6">
-          <h5>Friday</h5>
-          <img
-            source="https://static.thenounproject.com/png/1197734-200.png"
-            alt="Icon showing a sun and a cloud"
-            className="forecastEmoji"
-          />
-          <p>31°C | 18°C</p>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm">
+            <h5>Monday</h5>
+            <img
+              source={icon}
+              alt="Icon showing a sun and a cloud"
+              className="forecastEmoji"
+            />
+            <p>31°C | 18°C</p>
+          </div>
+          <div className="col-sm">
+            <h5>Tuesday</h5>
+            <img
+              source={icon}
+              alt="Icon showing a sun and a cloud"
+              className="forecastEmoji"
+            />
+            <p>31°C | 18°C</p>
+          </div>
+          <div className="col-sm">
+            <h5>Wednesday</h5>
+            <img
+              source={icon}
+              alt="Icon showing a sun and a cloud"
+              className="forecastEmoji"
+            />
+            <p>31°C | 18°C</p>
+          </div>
+          <div className="col-sm">
+            <h5>Thursday</h5>
+            <img
+              source={icon}
+              alt="Icon showing a sun and a cloud"
+              className="forecastEmoji"
+            />
+            <p>31°C | 18°C</p>
+          </div>
+          <div className="col-sm">
+            <h5>Friday</h5>
+            <img
+              source={icon}
+              alt="Icon showing a sun and a cloud"
+              className="forecastEmoji"
+            />
+            <p>31°C | 18°C</p>
+          </div>
         </div>
       </div>
     </div>

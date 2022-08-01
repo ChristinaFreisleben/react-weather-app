5b3d820de17f94fb434d87c27ae2bf3cc6e6ce15
Christina <98745726+ChristinaFreisleben@users.noreply.github.com>
2022-08-01 19:28:46 +0200

    Added forecast icon

--- src/Forecast.js
+++ src/Forecast.js
@@ -7,22 +7,47 @@ export default function Forecast() {
       <div className="row d-flex justify-content-center">
         <div className="col-lg-2 col-md-6">
           <h5>Monday</h5>
+          <img
+            source="https://cdn.dribbble.com/users/98494/screenshots/2714201/sun-icon.jpg"
+            alt="Icon showing a sun and a cloud"
+            className="forecastEmoji"
+          />
           <p>31°C | 18°C</p>
         </div>
         <div className="col-lg-2 col-md-6">
           <h5>Tuesday</h5>
+          <img
+            source="https://static.thenounproject.com/png/1197734-200.png"
+            alt="Icon showing a sun and a cloud"
+            className="forecastEmoji"
+          />
           <p>31°C | 18°C</p>
         </div>
         <div className="col-lg-2 col-md-6">
           <h5>Wednesday</h5>
+          <img
+            source="https://static.thenounproject.com/png/1197734-200.png"
+            alt="Icon showing a sun and a cloud"
+            className="forecastEmoji"
+          />
           <p>31°C | 18°C</p>
         </div>
         <div className="col-lg-2 col-md-6">
           <h5>Thursday</h5>
+          <img
+            source="https://static.thenounproject.com/png/1197734-200.png"
+            alt="Icon showing a sun and a cloud"
+            className="forecastEmoji"
+          />
           <p>31°C | 18°C</p>
         </div>
         <div className="col-lg-2 col-md-6">
           <h5>Friday</h5>
+          <img
+            source="https://static.thenounproject.com/png/1197734-200.png"
+            alt="Icon showing a sun and a cloud"
+            className="forecastEmoji"
+          />
           <p>31°C | 18°C</p>
         </div>
       </div>
